356f097c200d051b2b6999473c4d7ae572125844
siddhp09 <30808829+siddhp09@users.noreply.github.com>
2020-04-11 21:45:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,12 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 import pandas as pd
 
-# Link: https://www.worldometers.info/coronavirus/#countries
-
 service = Service('C:/Users/psidd/Downloads/chromedriver_win32 (1)/chromedriver.exe')
 service.start()
 driver = webdriver.Remote(service.service_url)
 driver.get("https://www.worldometers.info/coronavirus/#countries")
-time.sleep(20)  # Let the user actually see something!
+time.sleep(20)
+
 content = driver.page_source
 soup = BeautifulSoup(content, features="html.parser")
 names = []
@@ -19,7 +18,7 @@ total_Cases = []
 table = soup.find('table', attrs={'class': 'table table-bordered table-hover main_table_countries dataTable no-footer'})
 body = table.find('tbody')
 row = body.findAll('tr')
-index = 20
+index = 25
 
 for x in range(index):
     row_single = row[x]
@@ -28,27 +27,43 @@ for x in range(index):
     name = str(row_single.find('td').find('a'))
     indexF = name.find('>')
     end = name.find('<', indexF, len(name))
-    if name[indexF + 1:end] != 'Non':
+    if x == 0:
+        names.append("World")
+    elif x == 23:
+        names.append("Czechia")
+    elif x == 24:
+        names.append("Russia")
+    elif name[indexF + 1:end] != 'Non':
         names.append(name[indexF + 1:end])
 
-for x in range(len(row) - 20):
-    row_single = row[x + 20]
+for x in range(len(row) - 25):
+    row_single = row[x + 25]
     tDiv = row_single.find('td')
     name = str(tDiv.string)
     if name != 'None' and name != 'China' and name != 'Italy' and name != 'Iran' and name != 'Spain' and name != 'Germany' and name != 'USA' and name != 'France' and name != 'S. Korea' and name != 'UK':
         names.append(name.strip())
 
-for y in row:
-    cases = y.find('td', attrs={'class': 'sorting_1'})
+var = 0
+for y in range(len(row)):
+    row_single = row[y]
+    cases = row_single.find('td', attrs={'class': 'sorting_1'})
     number = str(cases.string)
     comma = number.find(',')
-    if comma != -1:
-        total_Cases.append(int(str(number[0:comma]) + str(number[comma + 1:len(number)])))
+    if var == 0:
+        var = var + 1
+        string1 = str(number[0:comma]) + str(number[comma + 1:len(number)])
+        comma2 = string1.find(',')
+        string2 = str(string1[0:comma2]) + str(string1[comma2 + 1:len(string1)])
+        total_Cases.append(int(str(string2)))
     else:
-        total_Cases.append(int(str(number)))
+        if comma != -1 and (y != 1 and y != 2 and y != 4 and y != 13 and y != 21 and y != 26 and y != 81) :
+            total_Cases.append(int(str(number[0:comma]) + str(number[comma + 1:len(number)])))
+        elif (y != 1 and y != 2 and y != 4 and y != 13 and y != 21 and y != 26 and y != 81) :
+            total_Cases.append(int(str(number)))
 
 excel_file = 'CV Latest.xlsx'
 df = pd.DataFrame({'Country': names, 'Total Cases': total_Cases})
 df.to_excel('CV Latest.xlsx', index=False, encoding='utf-8')
-
+writer = pd.ExcelWriter(excel_file, engine='xlsxwriter')
+df.to_excel(writer, sheet_name='Sheet1')
 
